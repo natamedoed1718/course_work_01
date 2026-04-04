008628f459465c1506e81d79ebf55236f9e8d979
natalakozlova <natamedoed22@gmail.com>
2026-04-04 20:08:51 +0300

    fixes views01

---
+++ src/config.py
@@ -0,0 +1,4 @@
+URL_CURRENCY = "https://api.apilayer.com/currency_data/convert"
+API_KEY = "PJ6a8n0Abldw947wy7Z0DYsdujkoqoOy"
+
+URL_STOCK = "https://www.alphavantage.co/query"
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from src.views import main, get_events
+
+if __name__ == "__main__":
+    print(main("2018-05-20 15:30:00"))
+
+if __name__ == "__main__":
+    print(get_events("2018-05-20 15:30:00"))
