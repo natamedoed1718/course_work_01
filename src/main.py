dfb3ecc843c6bbf05ba9f937747aab663bbdf92d
natalakozlova <natamedoed22@gmail.com>
2026-04-05 17:44:49 +0300

    ошибка главной и события

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from src.views import main, get_events
 
 if __name__ == "__main__":
-    print(main("2018-05-20 15:30:00"))
+    print(main("2021-11-24 12:00:00"))
+    print(get_events("2021-11-24 12:00:00"))
 
-if __name__ == "__main__":
-    print(get_events("2018-05-20 15:30:00"))
+
+
